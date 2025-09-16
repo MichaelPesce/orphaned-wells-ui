13ab43cb6dc9b6c5f13a7379d0f2d27f01f0d6a5
MichaelPesce <michaelcpesce@gmail.com>
2025-09-16 09:24:39 -0400

    add empty and loading table to projects list table

--- src/views/ProjectsListPage/ProjectsListPage.tsx
+++ src/views/ProjectsListPage/ProjectsListPage.tsx
@@ -7,23 +7,27 @@ import ProjectsListTable from '../../components/ProjectsListTable/ProjectsListTa
 import NewProjectDialog from '../../components/NewProjectDialog/NewProjectDialog';
 import ErrorBar from '../../components/ErrorBar/ErrorBar';
 import { Box } from '@mui/material';
+import { ProjectData } from '../../types';
 
 const ProjectsListPage = () => {
     const { userPermissions} = useUserContext();
-    const [projects, setProjects] = useState<any[]>([]);
+    const [projects, setProjects] = useState<ProjectData[]>([]);
     const [unableToConnect, setUnableToConnect] = useState(false);
     const [showNewProjectDialog, setShowNewProjectDialog] = useState(false);
     const [errorMsg, setErrorMsg] = useState<string | null>("")
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         callAPI(getProjects, [], handleSuccess, handleError);
     }, []);
 
     const handleSuccess = (data: any) => {
+        setLoading(false);
         setProjects(data);
     };
 
     const handleError = (e: Error) => {
+        setLoading(false);
         console.error(e);
         setUnableToConnect(true);
     };
@@ -52,7 +56,7 @@ const ProjectsListPage = () => {
             />
             <Box sx={styles.innerBox}>
                 {!unableToConnect ? 
-                    <ProjectsListTable projects={projects} />
+                    <ProjectsListTable projects={projects} loading={loading}/>
                 :
                     <h1>Unable to connect to backend. Please make sure that backend server is up and running.</h1>
                 }
