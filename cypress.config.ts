02b94efbd10cad931cbb9b960c2ae623ea028c39
MichaelPesce <michaelcpesce@gmail.com>
2026-08-11 09:37:10 -0400

    cleanup debug steps

--- cypress.config.ts
+++ cypress.config.ts
@@ -26,10 +26,6 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on("task", {
-        log(message) {
-          console.log(message);
-          return null;
-        },
         seedDatabase() {
           if (!config.env.resetDb) return null;
 
